4b4d3c8c9a2459b573e211ed1a693bc4008613ed
Dhruv Desai <dhruvdesai222@gmail.com>
2023-02-09 08:26:13 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# List
+
+my_list = ["Windows", "Linux", "MAC OS", "Unix"]
+print(type(my_list))
+
+# [NUMBER_OF_ELEMENTS] [INDEX_NUMBER]
+# Index in python starts from 0
+# That means the index of the first elememt is 0
+
+print('First Element', my_list[0])
+print('Lats Element', my_list[-1])
+
+print('Length', len(my_list))
