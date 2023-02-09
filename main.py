51d4364e3633eb6678f89baebd502b4881fb61b0
Dhruv Desai <dhruvdesai222@gmail.com>
2023-02-09 08:40:19 +0530

    Added the range part of list

--- main.py
+++ main.py
@@ -11,3 +11,29 @@ print('First Element', my_list[0])
 print('Lats Element', my_list[-1])
 
 print('Length', len(my_list))
+
+# Using Range
+
+# [START_POINT: END POINT]
+
+print(my_list[0:2])
+# 0
+# 0 + 1
+# 1 + 1 = 2 (Don't peint 2)
+
+
+# No o/p
+print(my_list[1:1])
+# 1 = 1
+
+print(my_list [-1:-2])
+print(my_list[-1:-2])
+
+# To add step size (By default it is set to 1)
+# [START_POINT: END POINT: STEP_SIZE]
+print(my_list)
+print(my_list[0:3:2])
+# o -> Windows
+# 0 + 2 -> Mac OS
+# 2 + 2 -> is out of range
+
